fbb0cc3adeb8d6acf442b65bdaafe65b08719fb8
Cristóvão Trevisan <cristovao@contabilizei.com.br>
2018-01-26 18:24:49 -0200

    Refactoring tests for better reading

--- src/index.test.js
+++ src/index.test.js
@@ -9,7 +9,6 @@ Object.defineProperties(window.HTMLElement.prototype, {
   },
 })
 
-
 const testHeights = [10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90]
 const testItems = testHeights.map(height => ({ name: `Test Item #${height}`, height }))
 const propsData = {
@@ -19,93 +18,71 @@ const propsData = {
   extraItems: 0,
 }
 
-test('should cache correct rendered height', (done) => {
+/**
+ * @returns {Promise.<void>}
+ */
+const updateList = list => new Promise((resolve) => {
+  list.update() // udpate number of items
+  Vue.nextTick(() => {
+    list.update() // update heights
+    Vue.nextTick(resolve)
+  })
+})
+
+test('should cache correct rendered height', () => {
   const testList = mount(TestList, { propsData })
 
-  // test initial render
   expect(testList.vm.$children[0].heights.length).toBe(0)
 
-  // test render after heights are read
-  testList.update() // udpate number of items
-  Vue.nextTick(() => {
-    testList.update() // update heights
-    Vue.nextTick(() => {
-      expect(testList.vm.$children[0].heights.length).toBe(10)
-      expect(testList.vm.$children[0].heights).toEqual(testHeights.slice(0, 10))
-      done()
-    })
+  return updateList(testList).then(() => {
+    expect(testList.vm.$children[0].heights.length).toBe(10)
+    expect(testList.vm.$children[0].heights).toEqual(testHeights.slice(0, 10))
   })
 })
 
-test('should calculate correct offset', (done) => {
+test('should calculate correct offset', () => {
   const testList = mount(TestList, { propsData })
 
-  // test initial render
   expect(testList.vm.$children[0].heights.length).toBe(0) // this includes empty divs
 
-  // test render after heights are read
-  testList.update() // udpate number of items
-  Vue.nextTick(() => {
-    testList.update() // update heights
-    Vue.nextTick(() => {
-      expect(testList.vm.$children[0].offset).toBe(0)
-      // set new scroll
-      testList.vm.$children[0].$el.scrollTop = 40
-      testList.vm.$children[0].$el.onscroll()
-      Vue.nextTick(() => {
-        expect(testList.vm.$children[0].offset).toBe(2)
-        done()
-      })
-    })
+  return updateList(testList).then(() => {
+    expect(testList.vm.$children[0].offset).toBe(0)
+    // set new scroll
+    testList.vm.$children[0].$el.scrollTop = 40
+    testList.vm.$children[0].$el.onscroll()
+    return updateList(testList).then(() => expect(testList.vm.$children[0].offset).toBe(2))
   })
 })
 
-test('should render only necessary elements', (done) => {
+test('should render only necessary elements', () => {
   const testList = mount(TestList, { propsData })
 
-  // test initial render
   expect(testList.vm.$el.children.length).toBe(12) // this includes empty divs
   expect(testList.vm.$children[0].$children.length).toBe(10) // only items rendered (100 / 10)
 
-  // test render after heights are read
-  testList.update() // udpate number of items
-  Vue.nextTick(() => {
-    testList.update() // update heights
-    Vue.nextTick(() => {
-      // only 5 items rendered (10 + 15 + 20 + 25 + 30 = 100)
-      expect(testList.vm.$children[0].$children.length).toBe(5)
-      done()
-    })
-  })
+  return updateList(testList)
+    .then(() => expect(testList.vm.$children[0].$children.length).toBe(5))
 })
 
-test('should reset when items length changes', (done) => {
+test('should reset when items length changes', () => {
   const testList = mount(TestList, { propsData })
 
-  // test initial render
   expect(testList.vm.$children[0].heights.length).toBe(0)
 
-  // test render after heights are read
-  testList.update() // udpate number of items
-  Vue.nextTick(() => {
-    testList.update() // update heights
-    Vue.nextTick(() => {
-      expect(testList.vm.$children[0].heights.length).toBe(10)
-      // set scroll
-      testList.vm.$children[0].$el.scrollTop = 40
-      testList.vm.$children[0].$el.onscroll()
+  return updateList(testList).then(() => new Promise((resolve) => {
+    expect(testList.vm.$children[0].heights.length).toBe(10)
+    // set scroll
+    testList.vm.$children[0].$el.scrollTop = 40
+    testList.vm.$children[0].$el.onscroll()
 
-      Vue.nextTick(() => {
-        // replace props
-        testList.setProps({ items: testItems.concat(95) })
-        Vue.nextTick(() => {
-          // test if reset worked
-          expect(testList.vm.$children[0].$el.scrollTop).toBe(0)
-          expect(testList.vm.$children[0].offset).toBe(0)
-          expect(testList.vm.$children[0].heights).toEqual([])
-          done()
-        })
-      })
+    // replace props
+    testList.setProps({ items: testItems.concat(95) })
+    Vue.nextTick(() => {
+      // test if reset worked
+      expect(testList.vm.$children[0].$el.scrollTop).toBe(0)
+      expect(testList.vm.$children[0].offset).toBe(0)
+      expect(testList.vm.$children[0].heights).toEqual([])
+      resolve()
     })
-  })
+  }))
 })
